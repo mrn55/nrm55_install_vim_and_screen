d40174f4e267efd007911b236895621298c5d9f5
Your Name <you@example.com>
2017-02-28 19:20:24 -0500

    nb: my script to help deploy my screenrc and vimrc faster

---
+++ nrm55_install_vim_and_screen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Running apt-get install for git, vim, screen"
+sudo apt-get install -y git vim screen
+echo "Done running apt-get"
+echo ""
+
+echo "Cloning screenrc script in"
+git clone https://github.com/nbla/screenrc.git
+echo "Creating sym link to screenrc/.screenrc in home dir"
+ln -s screenrc/.screenrc ~/.screenrc
+
+echo "Cloning vimrc script in"
+git clone https://github.com/nrm55/vimrc.git
+echo "Creating sym link to vimrc/.vimrc in home dir"
+ln -s vimrc/.vimrc ~/.vimrc
+
+echo "Creating ~/tmp dir for vim's swap files"
+mkdir ~/tmp
+echo "Done done"
